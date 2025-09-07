490556d0f54cc32ae205bf933d6ab97927ec5d15
tsuzuki-privateDev <tsuzuki2215@gmail.com>
2025-09-07 22:12:20 +0900

    feat: パラメータでページを切り替えるように修正

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -1,12 +1,19 @@
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/")
-    public String index() {
-        return "index";
+    @RequestMapping("/{temp}")
+    public String index(@PathVariable("temp") String temp) {    // コンパイラオプションで -parameters を有効化していないとValue属性いれないとエラーになる。あと、引数名とパス変数名が違うときも。
+        switch (temp) {
+            case "index":
+                return "index";
+        
+            default:
+                return "other";
+        }
     }
 }
