9e1d10a6a8ab5f38a2e3c0092bf50a876501ae9f
tsuzuki-privateDev <tsuzuki2215@gmail.com>
2025-09-07 18:49:57 +0900

    feat: ブラウザに「Hello, Spring Boot 3!!!」を表示する

--- src/main/java/com/example/sample1app/Sample1appApplication.java
+++ src/main/java/com/example/sample1app/Sample1appApplication.java
@@ -2,12 +2,19 @@ package com.example.sample1app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Sample1appApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sample1appApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	public String index() {
+		return "Hello, Spring Boot 3!!!";
+	}
 }
