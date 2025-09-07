27c142f97dfaef4aac32d90cf396c396bb8a5db0
tsuzuki-privateDev <tsuzuki2215@gmail.com>
2025-09-07 21:54:08 +0900

    change: Controllerに変更し、indexhtml追加。thymeleafパッケージ追加。

--- src/main/java/com/example/sample1app/Sample1appApplication.java
+++ src/main/java/com/example/sample1app/Sample1appApplication.java
@@ -3,35 +3,12 @@ package com.example.sample1app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
-@RestController
 public class Sample1appApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sample1appApplication.class, args);
-	}
-
-	@RequestMapping("/")
-	public String index(HttpServletRequest request, HttpServletResponse response) {
-		response.setContentType(MediaType.TEXT_HTML_VALUE);
-		String content = """
-			<html>
-				<head>
-				<title>Sample App</title>
-				</head>
-				<body>
-				<h1>Sample App</h1>
-				<p>This is sample app page!</p>
-			</html>
-			""";
-		return content;
 	}
 
 }
