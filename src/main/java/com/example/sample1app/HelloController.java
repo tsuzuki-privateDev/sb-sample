6cd321fd0b0426a3955ef3f3dd4cc1c0d97dbb60
tsuzuki-privateDev <tsuzuki2215@gmail.com>
2025-09-08 21:30:06 +0900

    feat: フォームコントロールの追加
    @RequestMapping
    @GetMapping
    @PostMapping
    @RequestParam

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -1,20 +1,24 @@
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/{num}")
-    public ModelAndView index(@PathVariable("num") int num, ModelAndView mav) {
-        int total = 0;
-        for (int i = 1; i < num; i++) {
-            total += i;
-        }
-        mav.addObject("msg", num + "までの合計を計算します。");
-        mav.addObject("content", "total: " + total);
+    @RequestMapping(value = "/", method = RequestMethod.GET)    // 引数が複数ある場合は、引数名を指定して書く   // @GetMapping("/") という書き方もできる
+    public ModelAndView index(ModelAndView mav) {
+        mav.addObject("msg", "名前を書いてください。");
+        mav.setViewName("index");
+        return mav;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)   // @PostMapping という書き方もできる
+    public ModelAndView form(@RequestParam("text1") String str, ModelAndView mav) {
+        mav.addObject("msg", "こんにちは、" + str + "さん！" );
+        mav.addObject("value", str);
         mav.setViewName("index");
         return mav;
     }
