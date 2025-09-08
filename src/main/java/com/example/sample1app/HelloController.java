aceef5862aa706c1ee964b916de88511d4e16604
tsuzuki-privateDev <tsuzuki2215@gmail.com>
2025-09-08 18:22:45 +0900

    feat: ThymeleafテンプレートとModelの利用を追加

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -1,19 +1,14 @@
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/{temp}")
-    public String index(@PathVariable("temp") String temp) {    // コンパイラオプションで -parameters を有効化していないとValue属性いれないとエラーになる。あと、引数名とパス変数名が違うときも。
-        switch (temp) {
-            case "index":
-                return "index";
-        
-            default:
-                return "other";
-        }
+    @RequestMapping()
+    public String index(Model model) {
+        model.addAttribute("msg", "これはコントローラーに用意したメッセージです。");
+        return "index";
     }
 }
