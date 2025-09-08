1a8be6d1f9cbf370453206d78c9706f40c3d6a20
tsuzuki-privateDev <tsuzuki2215@gmail.com>
2025-09-08 20:58:03 +0900

    feat: ModelAndView

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -1,19 +1,21 @@
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
     @RequestMapping("/{num}")
-    public String index(@PathVariable("num") int num, Model model) {
-        int res = 0;
+    public ModelAndView index(@PathVariable("num") int num, ModelAndView mav) {
+        int total = 0;
         for (int i = 1; i < num; i++) {
-            res += i;
+            total += i;
         }
-        model.addAttribute("msg", "total: " + res);
-        return "index";
+        mav.addObject("msg", num + "までの合計を計算します。");
+        mav.addObject("content", "total: " + total);
+        mav.setViewName("index");
+        return mav;
     }
 }
