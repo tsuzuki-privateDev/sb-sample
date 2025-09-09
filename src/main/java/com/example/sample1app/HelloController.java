e2213e45d369e4bfa70f85440ccdef0c434a32f1
tsuzuki-privateDev <tsuzuki2215@gmail.com>
2025-09-09 22:01:20 +0900

    feat: th:switchによるスイッチ処理

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -1,17 +1,17 @@
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/")
-    public ModelAndView index(ModelAndView mav) {
+    @RequestMapping("/{month}")
+    public ModelAndView index(@PathVariable("month") int month, ModelAndView mav) {
         mav.setViewName("index");
-        mav.addObject("msg", "データを表示します。");
-        String[] data = new String[] {"one", "two", "three"};
-        mav.addObject("data", data);
+        mav.addObject("msg", month + "月は？");
+        mav.addObject("month", month);
         return mav;
     }
 }
