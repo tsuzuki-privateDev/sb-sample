5eab2fd1b723882872a05fe34c1f28ad46a7347b
tsuzuki-privateDev <tsuzuki2215@gmail.com>
2025-09-10 20:52:03 +0900

    feat: 条件による表示(Mustache)
    ロジックレスが特徴のMustacheだが、if～else～のようなことも可能
    例：
    {{#flag}}
    ---trueの時表示---
    {{/flag}}
    {{^flag}}
    ---falseの時表示---
    {{/flag}}　　　

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -6,16 +6,14 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
+    private boolean flag = false;
+
     @RequestMapping("/")
     public ModelAndView index(ModelAndView mav) {
         mav.setViewName("index");
-        String msg = """
-                <div class="border border-primary">
-                 <h2>Message</h2>
-                 <p>This is sample message!</p>
-                </div>
-                """;
-        mav.addObject("msg", msg);
+        flag = !flag;
+        mav.addObject("flag", flag);
+        mav.addObject("msg", "メッセージを表示します。");
         return mav;
     }
 }
