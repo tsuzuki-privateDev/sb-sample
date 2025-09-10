44eb16e2e91eb6b926a7c80268d10c3f6258969e
tsuzuki-privateDev <tsuzuki2215@gmail.com>
2025-09-10 20:44:06 +0900

    feat: Mustacheテンプレートエンジンの利用
    値の表示は{{値}}
    HTMLをエスケープさせない{{{}}}

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -1,17 +1,21 @@
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/{month}")
-    public ModelAndView index(@PathVariable("month") int month, ModelAndView mav) {
+    @RequestMapping("/")
+    public ModelAndView index(ModelAndView mav) {
         mav.setViewName("index");
-        mav.addObject("msg", month + "月は？");
-        mav.addObject("month", month);
+        String msg = """
+                <div class="border border-primary">
+                 <h2>Message</h2>
+                 <p>This is sample message!</p>
+                </div>
+                """;
+        mav.addObject("msg", msg);
         return mav;
     }
 }
